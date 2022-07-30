fc04ef43d559085f4845a28707759d9cd4ef743c
rajesharaki <rajesharaki8613@gmail.com>
2022-07-30 20:04:44 +0530

    implemented uc1 code

---
+++ EmpWageProject/EmpWageProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] arg)
+    {
+        //Uc1 code
+
+        int isPresen = 1;
+
+        Random random = new Random();
+        int check = random.Next(0, 1);
+        if (check == isPresen)
+        {
+            Console.WriteLine("Emp Present");
+        }
+        else
+        {
+            Console.WriteLine("Emp Absent");
+        }
+    }
+}
